54cf8e7dba61e4ac7cfe15868b1ab2078640200b
lukas-zip <lukas.huberx1999@gmail.com>
2022-04-15 18:15:42 +0200

    navbar

--- tickets/app/routes.py
+++ tickets/app/routes.py
@@ -43,6 +43,7 @@ def login():
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
         return redirect(next_page)
+
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/logout')
