c4698bbd2ae4beebcca5a0c22de8e76ec6400ad2
Jonas Stoegerer <jonasstoegerer@heim-040-117.jkh.uni-linz.ac.at>
2022-05-05 16:15:16 +0200

    updated project

---
+++ Strecken/app/forms.py
@@ -0,0 +1,48 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from app.models import User
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
+
+class EmptyForm(FlaskForm):
+    submit = SubmitField('Submit')
+
+class EditStationForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    address = TextAreaField('Address', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class NewStationForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    address = TextAreaField('Address', validators=[DataRequired()])
+    submit = SubmitField('Submit')
---
+++ Strecken/app/routes.py
@@ -0,0 +1,191 @@
+from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user
+from app.models import User
+from app import app
+from app.forms import LoginForm, EmptyForm, EditStationForm, NewStationForm
+from flask_login import logout_user
+from flask_login import login_required
+from flask import request
+from werkzeug.urls import url_parse
+from app import db
+from app.forms import RegistrationForm
+from datetime import datetime
+from app.forms import EditProfileForm
+
+@app.route('/')
+@app.route('/index')
+@login_required
+def index():
+    # user = {'username': 'Jonas'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=current_user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    form = EmptyForm()
+    return render_template('user.html', user=user, posts=posts, form=form)
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)
+
+@app.route('/follow/<username>', methods=['POST'])
+@login_required
+def follow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            flash('User {} not found.'.format(username))
+            return redirect(url_for('index'))
+        if user == current_user:
+            flash('You cannot follow yourself!')
+            return redirect(url_for('user', username=username))
+        current_user.follow(user)
+        db.session.commit()
+        flash('You are following {}!'.format(username))
+        return redirect(url_for('user', username=username))
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/unfollow/<username>', methods=['POST'])
+@login_required
+def unfollow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            flash('User {} not found.'.format(username))
+            return redirect(url_for('index'))
+        if user == current_user:
+            flash('You cannot unfollow yourself!')
+            return redirect(url_for('user', username=username))
+        current_user.unfollow(user)
+        db.session.commit()
+        flash('You are not following {}.'.format(username))
+        return redirect(url_for('user', username=username))
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/stations')
+@login_required
+def stations():
+    # user = {'username': 'Jonas'}
+    stations = [
+        {
+            'name': 'Haag',
+            'address': 'Bahnhofstraße'
+        },
+        {
+            'name': 'Amstetten',
+            'address': 'Bahnhofstraße'
+        }
+    ]
+    return render_template('stations.html', title='Stations', user=current_user, stations=stations)
+
+# @app.route('/delete/<station>', methods=['POST'])
+# @login_required
+# def delete_station(station):
+#     form = EmptyForm()
+#     db.session.delete(station)
+#     db.session.commit()
+#     flash('Strecke deleted!')
+#     return redirect(url_for('routes.stations'))
+
+# @app.route('/edit_station', methods=['GET', 'POST'])
+# @login_required
+# def edit_station():
+#     form = EditStationForm()
+#     if form.validate_on_submit():
+#         current_user.username = form.username.data
+#         current_user.about_me = form.about_me.data
+#         db.session.commit()
+#         flash('Your changes have been saved.')
+#         return redirect(url_for('edit_profile'))
+#     elif request.method == 'GET':
+#         form.name.data = current_user.username
+#         form.address.data = current_user.about_me
+#     return render_template('edit_station.html', title='Edit Station',
+#                            form=form)
+
+@app.route('/edit_station', methods=['GET', 'POST'])
+@login_required
+def edit_station():
+    form = EditStationForm()
+    return render_template('edit_station.html', title='Edit Station', form=form)
+
+@app.route('/create_station', methods=['GET', 'POST'])
+@login_required
+def create_station():
+    form = NewStationForm()
+    return render_template('create_station.html', title='Edit Station', form=form)
